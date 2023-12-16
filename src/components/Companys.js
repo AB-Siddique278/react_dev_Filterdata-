366ce0c019cdfd41377a85752a79ebc39a1092f0
unknown <absiddique1060@gmail.com>
2023-12-16 14:43:44 +0600

    data add in table

--- src/components/Companys.js
+++ src/components/Companys.js
@@ -38,9 +38,10 @@ const Companys = () => {
 
     return (
         <>
-            <div>
+            {/* <div>
                 <label>Company Name:</label>
                 <input
+                    placeholder="Type here" className="input input-bordered input-info w-full max-w-xs"
                     type="text"
                     name="companyName"
                     value={filter.companyName}
@@ -55,20 +56,115 @@ const Companys = () => {
                     <option value="1">Active</option>
                     <option value="0">Inactive</option>
                 </select>
+            </div> */}
+
+
+            <div className='mb-10 gap-3 py-10'>
+                <label className='text-xs text-gray-700 uppercase font-bold'>Company Name: </label>
+                <input
+                    placeholder="Type here" className="input input-bordered w-full max-w-xs m-4"
+                    type="text"
+                    name="companyName"
+                    value={filter.companyName}
+                    onChange={handleFilterChange}
+                />
+                <label className='text-xs text-gray-700 uppercase font-bold '>Company Status: </label>
+                <select
+                    name="companyStatus"
+                    value={filter.companyStatus}
+                    onChange={handleFilterChange}
+                >
+                    <option value="1">Active</option>
+                    <option value="0">Inactive</option>
+                </select>
             </div>
 
             {loading ? <h1>Loading....</h1> : (
-                <div>
-                    
+                <div class="relative overflow-x-auto px-20 ">
 
 
 
-                    {companys && companys.data.map(company => (
-                        <div key={company.id}>
-                            <h1>{company.company_name}</h1>
-                           
-                        </div>
-                    ))}
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    ID
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Company Logo
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Company Name
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Company Phone
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Address 1
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Zip Code
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    City
+                                </th>
+                            </tr>
+                        </thead>
+
+
+                        <tbody>
+                            {companys && companys.data.map(company => (
+                                // <div key={company.id}>
+                                //     <h1>{company.company_name}</h1>
+                                //     <h1>{company.company_phone}</h1>
+
+                                // </div>
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {company.id}
+                                    </th>
+                                    <td class="px-6 py-4">
+
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={company.company_logo_link} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+
+                                        </div>
+
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {company.company_name}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {company.company_phone}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {company.address1}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {company.zip}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {company.city}
+                                    </td>
+                                </tr>
+                            ))}
+
+
+                        </tbody>
+
+
+
+
+
+
+
+                    </table>
+
+
+
+
 
 
                 </div>
